d2f629da6f0bb27d8208fc3290639d3548294109
권건율 <cjdak0907@naver.com>
2023-12-03 02:17:40 +0900

    추천,리뷰

--- Desktop/Here-Too/src/Main.java
+++ Desktop/Here-Too/src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args){
         SwingUtilities.invokeLater(() -> {
-            ReviewPanel frame = new ReviewPanel();
+            MainFrame frame = new MainFrame();
             frame.setVisible(true);
         });
     }
